9059a717f716717fd1d59f5264a741aaaf55f0e9
jiyoung-B <catgirl0313@naver.com>
2023-04-25 15:49:01 +0900

    스프링MVC 프로젝트 - 성적 조회 2

--- src/main/java/catgirl/spring4/sungjukv6/controller/SungjukController.java
+++ src/main/java/catgirl/spring4/sungjukv6/controller/SungjukController.java
@@ -3,8 +3,10 @@ package catgirl.spring4.sungjukv6.controller;
 import catgirl.spring4.sungjukv6.service.SungJukV6Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SungjukController {
@@ -16,8 +18,11 @@ public class SungjukController {
 
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public String list(){
+    public String list(Model model){
         System.out.println(sjsrv.readSungJuk());
+
+        // sungjuklist.jsp에 성적조회결과를 sjs라는 이름으로 넘김
+        model.addAttribute("sjs", sjsrv.readSungJuk());
         return "sungjuklist";
     }
 }
